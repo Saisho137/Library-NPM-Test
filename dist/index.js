43042c33b2571735fae7447f86ab38f2932e52f5
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-02-04 09:36:00 -0500

    fix: trying another configuration to allow export esm modules

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,4 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
 function greet(nombre) {
   return "Hola, ".concat(nombre, "!");
 }
-var _default = exports["default"] = greet;
+export default greet;
